ae7d34303efbab96ee94a9ba25e7404504898ab9
t9tieanh <phama9162@gmail.com>
2025-04-03 15:58:07 +0700

    update ui fe

--- fe_hotelmanagement/src/pages/CheckOut/PaymentPage/index.jsx
+++ fe_hotelmanagement/src/pages/CheckOut/PaymentPage/index.jsx
@@ -1,7 +1,12 @@
 import PrimaryButton from "../../../components/common/button/btn-primary"
+import CustomCard from "../../../components/common/Card"
+import { MdPayment } from "react-icons/md";
 
 const PaymentPage = ({handleNextStep}) => {
     return (<>
+
+        <CustomCard name={'Chọn phương thức thanh toán'} icon={<MdPayment />} />
+
         <PrimaryButton text={'Hoàn thành thanh toán'} onClickFunc={handleNextStep} />
     </>)
 }
